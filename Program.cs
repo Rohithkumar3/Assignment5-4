02639a5413ed133b9da3df1556b98425f04b82e1
Rohithkumar <rohithkumargande@gmail.com>
2023-11-09 15:50:38 +0530

    part4

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_part4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 101);
+            }
+
+            // Calculate the sum of elements in the array
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            // Calculate the average
+            double average = (double)sum / numbers.Length;
+
+            // Display sum and average
+            Console.WriteLine("Array of random integers:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nSum of the elements: " + sum);
+            Console.WriteLine("Average of the elements: " + average);
+
+            Console.ReadKey();
+        }
+            
+    }
+}
